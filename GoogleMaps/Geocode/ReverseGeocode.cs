97117d4b061949f91c2f4c2d5dad464a5479c21f
Paul Schütte <p.schuette@plexgroup.com>
2012-11-15 12:07:23 +0100

    Add option to set query-language.

--- GoogleMaps/Geocode/ReverseGeocode.cs
+++ GoogleMaps/Geocode/ReverseGeocode.cs
@@ -7,6 +7,8 @@ namespace GoogleMaps.Geocode {
 
 		private Location location;
 		private bool hasSensor;
+		private string defaultLanguage = "EN";
+
 		public ReverseGeocode() {
 			
 		}
@@ -20,32 +22,42 @@ namespace GoogleMaps.Geocode {
 			this.location = location;
 			this.hasSensor = hasSensor;
 		}
+		/// <summary>
+		/// Contains the results of the Async-Query after successful retrieval
+		/// </summary>
+		public ReverseGeocodeResult[] Results {
+			private set;
+			get;
+		}
 
 		public event EventHandler Updated;
 
 		public void QueryAsync() {
 			try {
-				QueryAsync(location);
+				QueryAsync(location.lat, location.lng, defaultLanguage);
 			} catch (Exception) {
 				throw new InvalidOperationException("Location is not set.");
 			}
 		}
-		public void QueryAsync(Location location) {
+		public void QueryAsync(string lat, string lng) {
+			QueryAsync(lat, lng, defaultLanguage);
+		}
+		public void QueryAsync(string lat, string lng, string language) {
 			var requestUri =
-				new Uri("http://maps.googleapis.com/maps/api/geocode/json?latlng=" + location.lat + "," + location.lng +
+				new Uri("http://maps.googleapis.com/maps/api/geocode/json?latlng=" + lat + "," + lng +
 				        "&sensor=" + hasSensor.ToString().ToLower());
 			var wr = WebRequest.CreateHttp(requestUri);
-			wr.Headers["Accept-Language"] = "DE";
+			wr.Headers["Accept-Language"] = language;
 			wr.BeginGetResponse(WebRequestReady, wr);
 		}
-
 		private void WebRequestReady(IAsyncResult asyncResult) {
 			var request = (HttpWebRequest)asyncResult.AsyncState;
 			var response = request.EndGetResponse(asyncResult);
 			var serializer = new DataContractJsonSerializer(typeof(ReverseGeocodeResults));
-			var objects = (ReverseGeocodeResults)serializer.ReadObject(response.GetResponseStream());
+			var results = (ReverseGeocodeResults)serializer.ReadObject(response.GetResponseStream());
+			Results = results.results;
 			if (null != Updated) {
-				Updated.Invoke(objects, null);
+				Updated.Invoke(this, null);
 			}
 		}
 	}
