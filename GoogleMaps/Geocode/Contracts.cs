d696e59a3f0b417645db524c8d10c2f1cb0b5675
Paul Schütte <p.schuette@plexgroup.com>
2012-11-15 11:22:44 +0100

    Initial commit.
    * Start implementing ReverseGeocode.

---
+++ GoogleMaps/Geocode/Contracts.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Net;
+using System.Runtime.Serialization;
+
+namespace GoogleMaps.Geocode {
+
+	[DataContract]
+	public class ReverseGeocodeResults {
+		[DataMember]
+		public ReverseGeocodeResult[] results { get; set; }
+	}
+
+	[DataContract]
+	public class ReverseGeocodeResult {
+		[DataMember]
+		public AddressComponent[] address_components { get; set; }
+
+		[DataMember]
+		public string formatted_address { get; set; }
+
+		[DataMember]
+		public GeocodeGeometry geometry { get; set; }
+
+		[DataMember]
+		public string[] types { get; set; }
+	}
+	[DataContract]
+	public struct AddressComponent {
+		[DataMember]
+		public string long_name { get; set; }
+
+		[DataMember]
+		public string short_name { get; set; }
+
+		[DataMember]
+		public string[] types { get; set; }
+	}
+	[DataContract]
+	public struct GeocodeGeometry {
+		[DataMember]
+		public Location location { get; set; }
+
+		[DataMember]
+		public string location_type { get; set; }
+
+		[DataMember]
+		public Viewport viewport { get; set; }
+
+		[DataMember]
+		public Viewport bounds { get; set; }
+	}
+	[DataContract]
+	public struct Location {
+		[DataMember]
+		public string lat { get; set; }
+		[DataMember]
+		public string lng { get; set; }
+	}
+	[DataContract]
+	public struct Viewport {
+		[DataMember]
+		public Location northeast { get; set; }
+
+		[DataMember]
+		public Location southwest { get; set; }
+	}
+}
---
+++ GoogleMaps/Geocode/ReverseGeocode.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Net;
+using System.Runtime.Serialization.Json;
+
+namespace GoogleMaps.Geocode {
+	public class ReverseGeocode {
+
+		private Location location;
+		private bool hasSensor;
+		public ReverseGeocode() {
+			
+		}
+		public ReverseGeocode(Location location) {
+			this.location = location;
+		}
+		public ReverseGeocode(bool hasSensor) {
+			this.hasSensor = hasSensor;
+		}
+		public ReverseGeocode(Location location, bool hasSensor) {
+			this.location = location;
+			this.hasSensor = hasSensor;
+		}
+
+		public event EventHandler Updated;
+
+		public void QueryAsync() {
+			try {
+				QueryAsync(location);
+			} catch (Exception) {
+				throw new InvalidOperationException("Location is not set.");
+			}
+		}
+		public void QueryAsync(Location location) {
+			var requestUri =
+				new Uri("http://maps.googleapis.com/maps/api/geocode/json?latlng=" + location.lat + "," + location.lng +
+				        "&sensor=" + hasSensor.ToString().ToLower());
+			var wr = WebRequest.CreateHttp(requestUri);
+			wr.Headers["Accept-Language"] = "DE";
+			wr.BeginGetResponse(WebRequestReady, wr);
+		}
+
+		private void WebRequestReady(IAsyncResult asyncResult) {
+			var request = (HttpWebRequest)asyncResult.AsyncState;
+			var response = request.EndGetResponse(asyncResult);
+			var serializer = new DataContractJsonSerializer(typeof(ReverseGeocodeResults));
+			var objects = (ReverseGeocodeResults)serializer.ReadObject(response.GetResponseStream());
+			if (null != Updated) {
+				Updated.Invoke(objects, null);
+			}
+		}
+	}
+}
